5d6809a32e91b0fd04359cfbf85725f96e447fea
SamuelRodriguesMGR <marat78880@gmail.com>
2024-12-05 10:05:54 +0500

    Initial commit

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 
+import os
+
 
 class Game:
     def __init__(self) -> None:
@@ -8,20 +10,30 @@ class Game:
         self.zero  : str = "O"
         self.cross : str = "X"
         
+        # Настоящий игрок
+        self.current_player : str = self.zero
+        
         # Генерация пустой матрицы [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
         self.box : list[list[str]] = [[self.empty for i in range(3)] for j in range(3)]
     
     def run(self) -> None:
         # Вызов обновления и вывод матрицы
         self.update_matrix()
-        
-        x, y = map(int, input(" > Введите координаты нолика (через пробел): ").split())
-        self.box[y][x] = self.zero
 
-        # Вызов обновления и вывод матрицы
-        self.update_matrix()
+        while True:
+            x, y = map(int, input(f" > Введите координаты {self.current_player} (через пробел): ").split())
+            self.box[y][x] = self.current_player
+
+            # Вызов обновления и вывод матрицы
+            self.update_matrix()
+
+            if self.current_player == self.zero: 
+                self.current_player = self.cross
+            else:
+                self.current_player = self.zero
 
     def update_matrix(self) -> None:
+        os.system("clear")
         for y in range(3):
             # Создаём список значений строки поля и объединяем её
             string = "|".join([self.box[y][x] for x in range(3)])
