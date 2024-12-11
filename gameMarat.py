31531be08a0e144a492d9320abe42241b93adc35
SamuelRodriguesMGR <marat78880@gmail.com>
2024-12-11 14:49:24 +0500

    Initial Commit

--- gameMarat.py
+++ gameMarat.py
@@ -1,6 +1,7 @@
 
 import pygame
 import random
+import time
 
 BLACK : int = (0, 0, 0)
 WHITE : int = (255, 255, 255)
@@ -13,30 +14,37 @@ class Game:
         pygame.init()
         pygame.display.set_caption("Pygame Tic Tac Toe")
 
-        self.SIZE_FIELD     : int  = 3
-        self.WIDTH          : int  = 900
+        size_field = input(" > Введите размер поля (от 1 до 10): ")
+
+        self.SIZE_FIELD     : int  = int(size_field) if size_field in [str(i) for i in range(1, 11)] else 3
         self.HEIGHT         : int  = 600
+        self.WIDTH          : int  = int(self.HEIGHT // self.SIZE_FIELD + self.HEIGHT)
         self.FPS            : int  = 30
-        self.CELL_SIZE      : int  = 200 # размер яйчеек
+        self.CELL_SIZE      : int  = self.HEIGHT // self.SIZE_FIELD # размер яйчеек
         self.LINE_THICKNESS : int  = 1   # размер толщины линии
-        self.LABEL_SIZE     : int  = 150 # размер иконок
-        self.field_cell     : list = [[[x, y, " "] for x in range(3)] for y in range(3)]
-        
-        self.order_steps : list = ["X", "O"]
-        random.shuffle(self.order_steps)
-        self.current_character : str = "player" if self.order_steps[0] == "O" else "bot"
-        self.winner            : str = ""
-        
+        self.LABEL_SIZE     : int  = int(self.CELL_SIZE * 0.75) # размер иконок
+
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock  = pygame.time.Clock()
 
         # фон
         self.Background = pygame.Surface((self.WIDTH, self.HEIGHT))
         self.Background.fill(BLACK)
+        
+        self._new_game()
 
+    def _new_game(self) -> None:
+        self.winner : str = ""
+        self.field_cell = [[[x, y, " "] for x in range(self.SIZE_FIELD)] for y in range(self.SIZE_FIELD)]
+        # рандом кто начинает
+        self.order_steps : list = ["X", "O"]
+        random.shuffle(self.order_steps)
+        self.current_character : str = "player" if self.order_steps[0] == "O" else "bot"
         # прикольная табличка
-        self.font = pygame.font.Font(None, 36)
+        self.size_font = int(self.CELL_SIZE * 0.18)
+        self.font = pygame.font.Font(None, self.size_font)
         self.current_text = "Удачной игры"
+        time.sleep(0.1)
 
     def run(self) -> None: 
 
@@ -49,18 +57,6 @@ class Game:
                 if event.type == pygame.QUIT: 
                     running = False
 
-            # ставим фон
-            self.screen.blit(self.Background, (0, 0))
-            # ставим текст
-            text_surface = self.font.render(self.current_text, True, WHITE)
-            text_rect = text_surface.get_rect(topleft=(self.CELL_SIZE * 3 + 40, 10))
-            self.screen.blit(text_surface, text_rect)
-
-            for row in self.field_cell:
-                for cell in row:
-                    self._update_map(cell)
-
-            pygame.display.flip()
 
             mouse_pressed  = pygame.mouse.get_pressed()
             mouse_position = pygame.mouse.get_pos()
@@ -68,33 +64,68 @@ class Game:
             y : int = int(mouse_position[1] / self.CELL_SIZE)
 
 
+            # ставим фон
+            self.screen.blit(self.Background, (0, 0))
+            # ставим текст
+            text_surface = self.font.render(self.current_text, True, WHITE)
+            text_rect = text_surface.get_rect(topleft=(self.CELL_SIZE * self.SIZE_FIELD + 5, 10))
+            self.screen.blit(text_surface, text_rect)
+            # ставим яйчейки
+            for row in self.field_cell:
+                for cell in row:
+                    self._update_map(cell)
+            # ставим кнопку рестарта
+            self.button_restart(self.SIZE_FIELD, self.SIZE_FIELD - 1, [x, y, mouse_pressed[0]])
+
+            pygame.display.flip()
+
+            # игровой процесс
             if mouse_pressed[0] and (x in range(self.SIZE_FIELD)) and (y in range(self.SIZE_FIELD)) and\
-                    self.field_cell[y][x][2] == " " and self.current_character == "player" and self.winner == "":
+                    self.field_cell[y][x][2] == " " and self.current_character == "player" and not self.winner:
                 self._put_label(x, y, "O")
+                
                 if not self._check_all_conditions():
                     self.winner = "player"
 
-            if self._check_all() and self.current_character == "bot" and self.winner == "":
+            if self._check_all() and self.current_character == "bot" and not self.winner:
                 self._step_bot()
+
                 if not self._check_all_conditions():
                     self.winner = "bot"
 
+             # победа
             if self.winner:
+               
                 if not self._check_hor() or not self._check_diag() or not self._check_vert():
-                    self._set_text(f"Победил : {self.winner}")
+                    self._set_text(f"Победил {self.winner}")
+
                 else:
                     self._set_text(f"Победила дружба")
 
-
             
         pygame.quit()
-    
-    def _update_map(self, cell) -> None:
-        # отрисовка каждой яйчейки
-        pygame.draw.rect(self.screen, WHITE, (cell[0] * self.CELL_SIZE, cell[1] * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE), self.LINE_THICKNESS)
 
+    def button_restart(self, x, y, mouse_data) -> None:
+        size_button_x, size_button_y = self.CELL_SIZE // 2, int(self.CELL_SIZE * 0.4)
+        center = self.CELL_SIZE // 2
+        pygame.draw.rect(self.screen, WHITE, 
+        (x * self.CELL_SIZE + center - size_button_x // 2, 
+         y * self.CELL_SIZE + center - size_button_y // 2, 
+        size_button_x, size_button_y))
+        # текст
+        text_surface = self.font.render("Рестарт", True, BLACK)
+        text_rect = text_surface.get_rect(topleft=(self.CELL_SIZE * x + center - size_button_x // 2 + 4, self.CELL_SIZE * y + center - size_button_y // 2 + 25))
+        self.screen.blit(text_surface, text_rect)
+        
+        if mouse_data[0] == x and mouse_data[1] == y and mouse_data[2]:
+            self._new_game()
+
+    def _update_map(self, cell) -> None:
         position_x = cell[0] * self.CELL_SIZE
         position_y = cell[1] * self.CELL_SIZE
+
+        # отрисовка каждой яйчейки
+        pygame.draw.rect(self.screen, WHITE, (position_x, position_y, self.CELL_SIZE, self.CELL_SIZE), self.LINE_THICKNESS)
 
         if cell[2] == "X":
             # Если коротко, это формула берёт центр яйчеки (position_x + self.CELL_SIZE // 2), а потом уводит x и y в правильные координаты
@@ -171,6 +202,7 @@ class Game:
 
     def _set_text(self, new_text : str) -> None:
         self.current_text = new_text
+
 
 if __name__ == "__main__":
     # Запуск игры
